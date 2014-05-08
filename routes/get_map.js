70fe7210008d0c72a9c13cb5d65d5ca8ca90d995
Amith Ananthram <amith.ananthram@gmail.com>
2014-05-08 17:46:54 -0400

    get map view done

--- routes/get_map.js
+++ routes/get_map.js
@@ -6,53 +6,13 @@ It then loads a map of campus with all the whiteboard locations marked (and clic
 var db = require('../models')
 
 module.exports = function(req, res) {
-	// db stuff should go here
-
-	//EventEmitter = require('events').EventEmitter;
-	//emitter = new EventEmitter();
-	//emitter.on('mouse_click', function(x, y) {
-	//	console.log('X: ' + x + 'Y: ' + y);
-	//})
-
-	// tests out users
-
-	/*var user = new db.User({
-		name: 'amith',
-		password: 'test'
-	});
-
-	user.save(function(err, user) {
-		if (err) throw err;
-		console.log(user);
-	});*/
-
-	/*db.User.find().exec(function(err, users) {
-		if (err) throw err;
-
-		console.log(users);
-	});*/
-	
-	// test whiteboard
-	/*
-	var whiteboard = new db.Whiteboard({
-		name: "test",
-		x_coordinate: 200,
-		y_coordinate: 150
-	});
-
-	whiteboard.save(function(err, whiteboard) {
-		if (err) throw err;
-		console.log(whiteboard);
-	});*/
-
 	// finds all the current whiteboards and passes it to the
 	// the campus map to be rendered
 	db.Messageboard.find().exec(function(err, messageboards) {
 		if (err) throw err;
 
-		res.render('map.ejs', { title: "Campus Map", messageboards : messageboards })
+		// note the call to JSON.stringify in the controller
+		// required to access JSON objects in <script> tags
+		res.render('map.ejs', { title: "Campus Map", messageboards : JSON.stringify(messageboards) })
 	});
-	
-	//var current_whiteboards = [[10, 1], [100, 100], [52, 70]];
-	//res.render('map.ejs', { title : "Campus Map", current_whiteboards : current_whiteboards });
 };
