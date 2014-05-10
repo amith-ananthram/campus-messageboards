3c41d56fad7f0ad4cdf0ad266c0799e342a1a3c2
Amith Ananthram <amith.ananthram@gmail.com>
2014-05-10 17:09:34 -0400

    error handling for posting

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Campus e-Whiteboards.
 Users can sign in (using a very basic authentication system) and create whiteboards at any location on the Dartmouth campus map or join an existing whiteboard.  Once there, users can leave messages on the board.
 
 TODO:
-- setup database and plug it into the views (done... just gotta make some views)
-- templating (<--- all that's left)
+- CSS to make the messageboard view and the forms prettier
+- 404 handling (invalid messageboard, etc)
--- routes/make_post.js
+++ routes/make_post.js
@@ -6,17 +6,29 @@ It then reloads the messageboard view.
 var db = require('../models')
 
 module.exports = function(req, res) {
+	// make sure there's a user logged in
 	if ( req.user != undefined ) {
-		var post = new db.Comment({
-		text : req.body.message,
-		posted_by_id : req.user._id,
-		posted_by_name : req.user.name,
-		messageboard : req.params.messageboard_id,
-		date : new Date()
-		});
+		// make sure there's content in the post
+		if ( req.body.message != "" ) {
+			// create a new post from the content
+			var post = new db.Comment({
+				text : req.body.message,
+				posted_by_id : req.user._id,
+				posted_by_name : req.user.name,
+				messageboard : req.params.messageboard_id,
+				date : new Date()
+			});
 
-		post.save();
-	};
+			post.save();
+		}
+		// if not, tell them to include content
+		else
+			res.error("Nothing to post!");
+	}
+	// if not, tell them they have to be logged in
+	else 
+		res.error("Must be logged in to post!");
 
-	res.redirect('/messageboards/' + req.params.messageboard_id);
+	// returns to the message board
+	res.redirect('back');
 }
